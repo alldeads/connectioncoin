30a4a36088ee0d33d494a6aa9edf7030713e435f
John Rexter Flores <johnrexter.flores@gmail.com>
2020-03-02 02:58:56 +0800

    Fix url error

--- resources/views/shared/notifications.blade.php
+++ resources/views/shared/notifications.blade.php
@@ -6,7 +6,12 @@
             <form method="POST" id="notif_form{{ $notification->id }}" action="/user/notifications">
                 @csrf
 
-                <input type="hidden" name="url" value="{{ $notification->data['url'] }}">
+                @if ( isset( $notification->data['url'] ) )
+
+                    <input type="hidden" name="url" value="{{ $notification->data['url'] }}">
+                @else
+                    <input type="hidden" name="url" value="/stories/{{ $notification->data['story_id'] }}">
+                @endif
 
                 <input type="hidden" name="notif_id" value="{{ $notification->id }}">
 
@@ -18,7 +23,16 @@
 
                 <a style="color: #3490dc !important; cursor: pointer;"
                      onclick="document.getElementById('notif_form{{ $notification->id }}').submit();"> 
+
+                    @if ( isset( $notification->data['message'] ) )
                         {{ $notification->data['message'] }}
+                    @else
+                        @if( $notification->type == "App\Notifications\StoryReact" )
+                            {{ $notification->data['user_first_name'] . " reacted on your story." }}
+                        @else
+                            {{ $notification->data['user_first_name'] . " commented on your story." }}
+                        @endif
+                    @endif
                 </a>
             </form>
         </li>
